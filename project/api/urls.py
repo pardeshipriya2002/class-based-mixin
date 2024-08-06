ef25059045549d31c83b2e96c7b63a61c8366913
priya <priyapardeshi610@gmail.com>
2024-08-06 18:31:38 +0530

    firstcommit

---
+++ project/api/urls.py
@@ -0,0 +1,14 @@
+
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+
+
+
+from django.urls import path
+from api import views
+
+urlpatterns = [
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+]
